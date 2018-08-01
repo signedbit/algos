ec1ecf9bcd17abb2d52d71853d898592202ddd20
Signedbit <signedbit@users.noreply.github.com>
2018-08-01 17:16:04 -0400

    Simplified LinkedList c'tor

--- src/main/java/net/signedbit/fun/ctci/MyNodeLinkedList.java
+++ src/main/java/net/signedbit/fun/ctci/MyNodeLinkedList.java
@@ -4,9 +4,7 @@ package net.signedbit.fun.ctci;
 public class MyNodeLinkedList<T> extends MyLinkedList<MyLinkedList.Node<T>> {
     @SafeVarargs
     public MyNodeLinkedList(final Node<T>... nodes) {
-        for (final Node<T> node : nodes) {
-            append(node);
-        }
+        super(nodes);
     }
 
     @Override
