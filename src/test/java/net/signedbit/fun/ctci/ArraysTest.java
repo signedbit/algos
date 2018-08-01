b444bbc4af3883f425e215c8d004f13e9eb51c47
Signedbit <signedbit@users.noreply.github.com>
2018-08-01 18:44:20 -0400

    Reformat ArraysTest for readability

--- src/test/java/net/signedbit/fun/ctci/ArraysTest.java
+++ src/test/java/net/signedbit/fun/ctci/ArraysTest.java
@@ -6,91 +6,91 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ArraysTest {
+    private final Arrays arrays = new Arrays();
 
     @Test
     void isUnique() {
-        assertTrue(new Arrays().isUnique("a"));
-        assertTrue(new Arrays().isUnique(""));
-        assertTrue(new Arrays().isUnique(null));
-        assertTrue(new Arrays().isUnique("ab"));
-        assertTrue(new Arrays().isUnique("abc"));
-        assertFalse(new Arrays().isUnique("aa"));
-        assertFalse(new Arrays().isUnique("aab"));
-        assertFalse(new Arrays().isUnique("abbc"));
-        assertFalse(new Arrays().isUnique("abcc"));
+        assertTrue(arrays.isUnique("a"));
+        assertTrue(arrays.isUnique(""));
+        assertTrue(arrays.isUnique(null));
+        assertTrue(arrays.isUnique("ab"));
+        assertTrue(arrays.isUnique("abc"));
+        assertFalse(arrays.isUnique("aa"));
+        assertFalse(arrays.isUnique("aab"));
+        assertFalse(arrays.isUnique("abbc"));
+        assertFalse(arrays.isUnique("abcc"));
     }
 
     @Test
     void isPermutation() {
-        assertTrue(new Arrays().isPermutation("", ""));
-        assertTrue(new Arrays().isPermutation("a", "a"));
-        assertTrue(new Arrays().isPermutation("ab", "ab"));
-        assertTrue(new Arrays().isPermutation("ab", "ba"));
-        assertTrue(new Arrays().isPermutation("abc", "cba"));
-        assertTrue(new Arrays().isPermutation("abc", "cab"));
-        assertFalse(new Arrays().isPermutation(null, null));
-        assertFalse(new Arrays().isPermutation(null, "a"));
-        assertFalse(new Arrays().isPermutation("a", null));
-        assertFalse(new Arrays().isPermutation("abc", "abcd"));
-        assertFalse(new Arrays().isPermutation("", "a"));
+        assertTrue(arrays.isPermutation("", ""));
+        assertTrue(arrays.isPermutation("a", "a"));
+        assertTrue(arrays.isPermutation("ab", "ab"));
+        assertTrue(arrays.isPermutation("ab", "ba"));
+        assertTrue(arrays.isPermutation("abc", "cba"));
+        assertTrue(arrays.isPermutation("abc", "cab"));
+        assertFalse(arrays.isPermutation(null, null));
+        assertFalse(arrays.isPermutation(null, "a"));
+        assertFalse(arrays.isPermutation("a", null));
+        assertFalse(arrays.isPermutation("abc", "abcd"));
+        assertFalse(arrays.isPermutation("", "a"));
     }
 
     @Test
     void urlify() {
-        assertEquals("", new Arrays().urlify("", 0));
-        assertEquals("Hi", new Arrays().urlify("Hi", 2));
-        assertEquals("H%20i", new Arrays().urlify("H i", 5));
-        assertEquals("%20i", new Arrays().urlify(" i", 4));
-        assertEquals("H%20", new Arrays().urlify("H ", 4));
-        assertEquals("Mr%20John%20Smith", new Arrays().urlify("Mr John Smith", 17));
+        assertEquals("", arrays.urlify("", 0));
+        assertEquals("Hi", arrays.urlify("Hi", 2));
+        assertEquals("H%20i", arrays.urlify("H i", 5));
+        assertEquals("%20i", arrays.urlify(" i", 4));
+        assertEquals("H%20", arrays.urlify("H ", 4));
+        assertEquals("Mr%20John%20Smith", arrays.urlify("Mr John Smith", 17));
     }
 
     @Test
     void isPalindromePermutation() {
-        assertTrue(new Arrays().isPalindromePermutation(""));
-        assertTrue(new Arrays().isPalindromePermutation("a"));
-        assertTrue(new Arrays().isPalindromePermutation("aba"));
-        assertTrue(new Arrays().isPalindromePermutation("aab"));
-        assertTrue(new Arrays().isPalindromePermutation("Tact Coa"));
-        assertTrue(new Arrays().isPalindromePermutation("Racecar"));
-        assertTrue(new Arrays().isPalindromePermutation("Racecra"));
-        assertTrue(new Arrays().isPalindromePermutation("Raccar"));
-        assertTrue(new Arrays().isPalindromePermutation("Ractcar"));
-        assertTrue(new Arrays().isPalindromePermutation("Ractcra"));
-        assertFalse(new Arrays().isPalindromePermutation(null));
-        assertFalse(new Arrays().isPalindromePermutation("ab"));
-        assertFalse(new Arrays().isPalindromePermutation("Taco"));
-        assertFalse(new Arrays().isPalindromePermutation("Tacoca"));
+        assertTrue(arrays.isPalindromePermutation(""));
+        assertTrue(arrays.isPalindromePermutation("a"));
+        assertTrue(arrays.isPalindromePermutation("aba"));
+        assertTrue(arrays.isPalindromePermutation("aab"));
+        assertTrue(arrays.isPalindromePermutation("Tact Coa"));
+        assertTrue(arrays.isPalindromePermutation("Racecar"));
+        assertTrue(arrays.isPalindromePermutation("Racecra"));
+        assertTrue(arrays.isPalindromePermutation("Raccar"));
+        assertTrue(arrays.isPalindromePermutation("Ractcar"));
+        assertTrue(arrays.isPalindromePermutation("Ractcra"));
+        assertFalse(arrays.isPalindromePermutation(null));
+        assertFalse(arrays.isPalindromePermutation("ab"));
+        assertFalse(arrays.isPalindromePermutation("Taco"));
+        assertFalse(arrays.isPalindromePermutation("Tacoca"));
     }
 
     @Test
     void isOneAway() {
-        assertTrue(new Arrays().isOneAway("pale", "ple"));
-        assertTrue(new Arrays().isOneAway("pales", "pale"));
-        assertTrue(new Arrays().isOneAway("pale", "pales"));
-        assertTrue(new Arrays().isOneAway("pale", "bale"));
-        assertFalse(new Arrays().isOneAway(null, "pale"));
-        assertFalse(new Arrays().isOneAway("pale", null));
-        assertFalse(new Arrays().isOneAway(null, null));
-        assertFalse(new Arrays().isOneAway("pale", "bake"));
+        assertTrue(arrays.isOneAway("pale", "ple"));
+        assertTrue(arrays.isOneAway("pales", "pale"));
+        assertTrue(arrays.isOneAway("pale", "pales"));
+        assertTrue(arrays.isOneAway("pale", "bale"));
+        assertFalse(arrays.isOneAway(null, "pale"));
+        assertFalse(arrays.isOneAway("pale", null));
+        assertFalse(arrays.isOneAway(null, null));
+        assertFalse(arrays.isOneAway("pale", "bake"));
     }
 
     @Test
     void compress() {
-        assertEquals(null, new Arrays().compress(null));
-        assertEquals("a2b1c5a3", new Arrays().compress("aabcccccaaa"));
-        assertEquals("a", new Arrays().compress("a"));
-        assertEquals("abb", new Arrays().compress("abb"));
-        assertEquals("abbcc", new Arrays().compress("abbcc"));
-        assertEquals("abbccc", new Arrays().compress("abbccc"));
-        assertEquals("a1b2c4", new Arrays().compress("abbcccc"));
+        assertEquals(null, arrays.compress(null));
+        assertEquals("a2b1c5a3", arrays.compress("aabcccccaaa"));
+        assertEquals("a", arrays.compress("a"));
+        assertEquals("abb", arrays.compress("abb"));
+        assertEquals("abbcc", arrays.compress("abbcc"));
+        assertEquals("abbccc", arrays.compress("abbccc"));
+        assertEquals("a1b2c4", arrays.compress("abbcccc"));
     }
 
     @Test
     void rotateMatrix() {
-        assertEquals(null, new Arrays().rotateMatrix(null));
-//        assertEquals(null, new Arrays().rotateMatrix(new int[1][2]));
-        assertArrayEquals(new int[][]{{3, 2}, {4, 1}}, new Arrays().rotateMatrix(new int[][]{
+        assertNull(arrays.rotateMatrix(null));
+        assertArrayEquals(new int[][]{{3, 2}, {4, 1}}, arrays.rotateMatrix(new int[][]{
                 {2, 1},
                 {3, 4}
         }));
@@ -98,32 +98,57 @@ class ArraysTest {
 
     @Test
     void zeroMatrix() {
-        assertEquals(null, new Arrays().rotateMatrix(null));
-        assertArrayEquals(new int[][]{{2, 1, 2}, {3, 4, 12}, {4, 45, 20}}, new Arrays().zeroMatrix(new int[][]{
+        assertNull(arrays.zeroMatrix(null));
+
+        // case 1: no rows or cols are zeroed
+        int[][] expected = {
                 {2, 1, 2},
                 {3, 4, 12},
                 {4, 45, 20}
-        }));
-        assertArrayEquals(new int[][]{{2, 0, 2}, {0, 0, 0}, {4, 0, 20}}, new Arrays().zeroMatrix(new int[][]{
+        };
+        int[][] actual = arrays.zeroMatrix(new int[][]{
+                {2, 1, 2},
+                {3, 4, 12},
+                {4, 45, 20}
+        });
+        assertArrayEquals(expected, actual);
+
+
+        // case 2: the center row and column is zeroed
+        expected = new int[][]{
+                {2, 0, 2},
+                {0, 0, 0},
+                {4, 0, 20}
+        };
+        actual = arrays.zeroMatrix(new int[][]{
                 {2, 1, 2},
                 {3, 0, 12},
                 {4, 45, 20}
-        }));
-        assertArrayEquals(new int[][]{{0, 1, 2}, {0, 4, 12}, {0, 0, 0}}, new Arrays().zeroMatrix(new int[][]{
+        });
+        assertArrayEquals(expected, actual);
+
+
+        // case 3: a non-center row and column is zeroed
+        expected = new int[][]{
+                {0, 1, 2},
+                {0, 4, 12},
+                {0, 0, 0}
+        };
+        actual = arrays.zeroMatrix(new int[][]{
                 {2, 1, 2},
                 {3, 4, 12},
                 {0, 5, 20}
-        }));
+        });
+        assertArrayEquals(expected, actual);
     }
 
     @Test
     void isRotated() {
-        assertTrue(new Arrays().isRotated("erbottlewat", "waterbottle"));
-        assertTrue(new Arrays().isRotated("rbottlewate", "waterbottle"));
-        assertTrue(new Arrays().isRotated("ewaterbottl", "waterbottle"));
-        assertFalse(new Arrays().isRotated(null, "waterbottle"));
-        assertFalse(new Arrays().isRotated("erbottlewat", null));
-        assertFalse(new Arrays().isRotated(null, null));
-
+        assertTrue(arrays.isRotated("erbottlewat", "waterbottle"));
+        assertTrue(arrays.isRotated("rbottlewate", "waterbottle"));
+        assertTrue(arrays.isRotated("ewaterbottl", "waterbottle"));
+        assertFalse(arrays.isRotated(null, "waterbottle"));
+        assertFalse(arrays.isRotated("erbottlewat", null));
+        assertFalse(arrays.isRotated(null, null));
     }
 }
