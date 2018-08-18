8c62de8e7a6cf2f07a9e7535bef1aa06c4495a30
Signedbit <signedbit@users.noreply.github.com>
2018-08-18 13:05:47 -0700

    Fixed SortedSearch so it works in many more situations.

--- src/main/java/net/signedbit/fun/screening/SortedSearch.java
+++ src/main/java/net/signedbit/fun/screening/SortedSearch.java
@@ -1,5 +1,7 @@
 package net.signedbit.fun.screening;
 
+import com.google.common.primitives.Ints;
+
 import java.util.Arrays;
 
 public class SortedSearch {
@@ -7,15 +9,44 @@ public class SortedSearch {
      * Given a sorted array, the goal is to determine how many elements in the array
      * are strictly less than a certain number.
      *
-     * Time: O(log n)
+     * Time: O(log n + n/2-1) = O(log n + n) = O(n), but O(log n) in many cases.
      * Space: O(1)
      */
-    public static int countNumbers(int[] sortedArray, int lessThan) {
-        final int index = Arrays.binarySearch(sortedArray, lessThan);
-        if (index < 0) {
-            return -(index + 1);
+    public static int countNumbers(int[] sortedArray, int key) {
+        final int length = sortedArray.length;
+        if (length == 0) {
+            // no numbers, nothing can be < key
+            return 0;
         }
-        return index;
+        if (key <= sortedArray[0]) {
+            // no numbers are less than key
+            return 0;
+        }
+        if (key > sortedArray[length - 1]) {
+            // everything is < key
+            return length;
+        }
+
+        int index = Arrays.binarySearch(sortedArray, key);
+        if (index < 0) {
+            // From Arrays#binarySearch:
+            //
+            // "If the array contains multiple elements with the specified value,
+            // there is no guarantee which one will be found."
+            //
+            // Therefore we must determine if we are at the beginning of the run.
+            index = -(index + 1); // determine insertion point
+        }
+
+        final int num = sortedArray[index]; // element at insertion point
+
+        if (key != num) {
+            // key should be where num is located
+            return index;
+        }
+
+        // scan left for first instance of "key"
+        return Ints.indexOf(sortedArray, key);
     }
 
     public static void main(String[] args) {
